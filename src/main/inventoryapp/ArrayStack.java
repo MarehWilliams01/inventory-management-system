f2a4baeca170392a21d377d50f950d1e0e5e8ada
Mareh Williams <williamsmareh@gmail.com>
2023-07-20 09:35:28 +0000

    Update ArrayStack.java

--- src/main/inventoryapp/ArrayStack.java
+++ src/main/inventoryapp/ArrayStack.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.group9.inventoryapp;
 
 import java.util.Arrays;
 /**
  *
- * @author erica selasie
  * @param <T>
  */
 public class ArrayStack<T> implements StackADT<T>{
