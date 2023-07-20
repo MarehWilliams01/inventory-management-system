5be6fa756fd26df4199b4d0dce75e98a7c1e5de1
Mareh Williams <williamsmareh@gmail.com>
2023-07-20 09:41:00 +0000

    Update products.java

--- src/main/inventoryapp/products.java
+++ src/main/inventoryapp/products.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-package com.group9.inventoryapp;
+
+package com.inventoryapp;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 import java.sql.Connection;
