cd2312903119d713d0d02511510f3c80f48f2e27
Mareh Williams <williamsmareh@gmail.com>
2023-07-20 09:35:51 +0000

    Update InventoryApp.java

--- src/main/inventoryapp/InventoryApp.java
+++ src/main/inventoryapp/InventoryApp.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.group9.inventoryapp;
 
 /**
  *
- * @author erica selasie
  */
 public class InventoryApp {
 
